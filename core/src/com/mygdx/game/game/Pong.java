5e6cf745098993e4fa11f9b384851f2fe8fbfdfd
DavidMartinez <d.martinezgarc@alumnos.urjc.es>
2018-02-10 17:57:50 +0100

    Actualizar Comentario

--- core/src/com/mygdx/game/game/Pong.java
+++ core/src/com/mygdx/game/game/Pong.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.Game;
 import com.mygdx.game.screens.GameScreen;
 
 public class Pong extends Game {
-// Proyecto Dos: Comentario para rama HU13T1 P1
+// Proyecto Dos: Comentario para rama HU13T1 P1 P2
     @Override
     public void create() {
         setScreen(new GameScreen());
