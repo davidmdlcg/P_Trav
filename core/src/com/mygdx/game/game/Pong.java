ac1ed6d393784c0088969d7fb0f36eea3faa3f40
DavidMartinez <d.martinezgarc@alumnos.urjc.es>
2018-02-10 14:38:46 +0100

    Commit del proyecto 2

--- core/src/com/mygdx/game/game/Pong.java
+++ core/src/com/mygdx/game/game/Pong.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.Game;
 import com.mygdx.game.screens.GameScreen;
 
 public class Pong extends Game {
-
+// Proyecto Dos: Comentario para rama HU13T1 P1
     @Override
     public void create() {
         setScreen(new GameScreen());
