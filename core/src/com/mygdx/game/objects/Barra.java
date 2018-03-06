2e09dd0dfecd310937881054535a466c59413672
David <d.martinezgarc@alumnos.urjc.es>
2018-03-06 23:22:39 +0100

    Descomponer Condicional

--- core/src/com/mygdx/game/objects/Barra.java
+++ core/src/com/mygdx/game/objects/Barra.java
@@ -36,11 +36,11 @@ public class Barra extends FiguraGrafica {
                 getPosition().add(getVelocity().cpy().scl(delta));
             }
         } else {//si no es ninguna la velocidad actual se convierte en la contraria y la pinta
-            this.getPosition().x =(float)this.widhtT - this.width;
+            this.getPosition().x =(float)Gdx.graphics.getWidth() - this.width;
             getPosition().add(getVelocity().cpy().scl(delta));
 
         }
-        if (this.getPosition().x + this.width < this.widhtT) {// si la pala esta a la derecha de la pala la pinta
+        if (this.getPosition().x + this.width < Gdx.graphics.getWidth()) {// si la pala esta a la derecha de la pala la pinta
             if (this.getVelocity().x > 0) {//si la pala se mueve la pinte
                 getPosition().add(getVelocity().cpy().scl(delta));
             }
@@ -51,27 +51,48 @@ public class Barra extends FiguraGrafica {
     }
 
     protected void chocarEnY(float delta) {
-        if (this.getY() >= -1) {//si la parte de arriba de la pala es menor que 0 pinta la bola
-            if (this.getVelocity().y < 0) {//si la pala se mueve que la pinte
-                getPosition().add(getVelocity().cpy().scl(delta));
-            }
-        } else {//si no es ninguna la velocidad actual se convierte en la contraria y la pinta
-            this.setY((float) Gdx.graphics.getHeight()-this.height);
+        if (estaPorEncimaDelBordeInferior()) {
             getPosition().add(getVelocity().cpy().scl(delta));
+        } else {
+            moverABordeSuperior(delta);
         }
-        if (this.getY() + this.height < this.heightT) {//si la parte de abajo de la pala es amyor que la altura la pinta
-            if (this.getVelocity().y > 0) {//si la pala se mueve pinta la bola
-                getPosition().add(getVelocity().cpy().scl(delta));
-            }
-        } else {//si no es ninguna la velocidad actual se convierte en la contraria y la pinta
-            this.setY(1);
+        if (estaPorDebajoDelBordeSuperior()) {
             getPosition().add(getVelocity().cpy().scl(delta));
+        } else {
+            moverABordeInferior(delta);
         }
     }
 
-    protected void cambiarPosicionYSiLimite(float delta) {
+    private void moverABordeInferior(float delta) {
+        this.setY(1);
+        getPosition().add(getVelocity().cpy().scl(delta));
+    }
+
+    private void moverABordeSuperior(float delta) {
+        this.setY((float)( Gdx.graphics.getHeight())-this.height);
+        getPosition().add(getVelocity().cpy().scl(delta));
+    }
+
+    protected boolean estaPorDebajoDelBordeSuperior() {
+        return this.getY() + this.height < Gdx.graphics.getHeight();
+    }
+
+    protected boolean estaMoviendoseHaciaArriba(){
+        return this.getVelocity().y > 0;
+    }
+
+
+    protected boolean estaMoviendoseHaciaAbajo(){
+        return this.getVelocity().y < 0;
+    }
+
+    protected boolean estaPorEncimaDelBordeInferior(){
+        return this.getPosition().y > 0;
+    }
+
+    private void cambiarPosicionYSiLimite(float delta) {
         if(this.getPosition().y == 0){
-            this.getPosition().y =(float)this.widhtT - this.width;
+            this.getPosition().y =(float)Gdx.graphics.getWidth() - this.width;
             getPosition().add(getVelocity().cpy().scl(delta));
         }
     }
